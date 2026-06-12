e56b37861446cdc5428be56519fae83cd52ef478
Riku Räsänen <riku.rasanen@10duke.com>
2026-06-12 14:58:17 +0300

    Add events fired when access to an Entitlement has been granted to or revoked from an Organization Group

---
+++ src/main/java/com/tenduke/events/api/model/data/EntitlementRevokedFromOrganizationGroup.java
@@ -0,0 +1,7 @@
+package com.tenduke.events.api.model.data;
+
+/**
+ * Fired when access to an Entitlement has been granted to an OrganizationGroup.
+ *
+ */
+public interface EntitlementRevokedFromOrganizationGroup extends EntitlementAccessChanged {}
